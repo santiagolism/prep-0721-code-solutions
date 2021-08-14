c4827731a5d4d3a3b06b7f70fa2fbc1171ecfe70
santiagolism <santiagolism96@gmail.com>
2021-08-13 23:03:13 -0700

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/prep-javascript-objects/main.js
@@ -0,0 +1,8 @@
+const person = { firstName: 'Santiago', lastName: 'Lis', hoby: 'Gaming', job: 'Seller', previousJob: 'Baker' };
+var firstName = 'Santiago';
+var lastName = ' Lis';
+var fullName = firstName + lastName;
+console.log("The person's name", 'is:', fullName);
+console.log("The peron's current job" + ' is: ' + person.job);
+console.log("The person's previous job" + ' is: ' + person.previousJob);
+console.log(person);
